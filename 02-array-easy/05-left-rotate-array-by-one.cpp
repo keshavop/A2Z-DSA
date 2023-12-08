68ba4b39f5c11d5bf20e5ecdb4562d8d8e4b0c44
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2023-12-08 16:33:10 +0530

    Update 05-left-rotate-array-by-one.cpp

--- 02-array-easy/05-left-rotate-array-by-one.cpp
+++ 02-array-easy/05-left-rotate-array-by-one.cpp
@@ -10,3 +10,11 @@ return arr;
 }
 
 //codestudio efficient approach
+vector<int> rotateArray(vector<int>& arr, int n) {
+    vector<int> temp(n);
+    for(int i=1;i<n;i++){
+        temp[i-1] = arr[i];
+    }
+    temp[n-1] = arr[0];
+    return temp;
+}
