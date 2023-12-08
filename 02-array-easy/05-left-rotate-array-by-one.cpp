0182302639781eb4bd8f258b3c3b24e92f97f85e
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2023-12-08 10:59:06 +0530

    left rotate array by one

---
+++ 02-array-easy/05-left-rotate-array-by-one.cpp
@@ -0,0 +1,12 @@
+//codestudio brute approach
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) {
+    int temp = arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1] = temp;
+return arr;
+}
+
+//codestudio efficient approach
