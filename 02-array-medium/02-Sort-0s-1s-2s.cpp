61007c63b09811d04383a7c6b0142027dccf14c0
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2023-12-22 12:39:41 +0530

    sort 0 1 2

--- 02-array-medium/02-Sort-0s-1s-2s.cpp
+++ 02-array-medium/02-Sort-0s-1s-2s.cpp
@@ -1,4 +1,24 @@
-// codestudio
-
-
-//leetcode
+// codestudio && leetcode
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+    int low = 0;
+    int high = nums.size()-1;
+    int mid = 0;
+    
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
+    }
+    }
+};
