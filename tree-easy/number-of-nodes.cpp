d9ad2664bde28a9806f8d44d891d96a6c38a3bb1
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2024-01-30 23:01:27 +0530

    number of nodes

---
+++ tree-easy/number-of-nodes.cpp
@@ -0,0 +1,5 @@
+// https://www.codingninjas.com/studio/problems/number-of-nodes_8162204?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+int numberOfNodes(int N){
+    return pow(2, N-1);
+}
